4853a1a343c7653dd15d39de188bff31608cea7d
LuisGonzalez27 <luis27coding@gmail.com>
2022-10-26 19:24:29 +0200

    test su vue

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 "use strict";
-
-
 // console.log(Vue);
 
 const { createApp } = Vue;
@@ -9,8 +7,18 @@ const myApp = createApp({
     data() {
         return {
             message: 'Hello World !',
-            myImage : './img/Logo.png'
+            myImage : './img/Logo.png',
+            myClass : 'green',
+            count: 0
         }
-    }
+    },
+        methods: {
+            changeColor(){
+                this.myClass = 'blue display-6';
+            },
+            colorFirst(){
+                this.myClass = 'green';
+            }
+        }
 });
 myApp.mount('#myApp');
